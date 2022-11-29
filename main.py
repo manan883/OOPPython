cd9cb3d4e1036ffe58e3ae3ac9bddfbff74ead0f
manan883 <manan883@gmail.com>
2022-11-28 22:14:32 -0800

    made a rudementary user interface

--- main.py
+++ main.py
@@ -66,6 +66,7 @@ class exam(question):
         uInput = input()
         self.userAns.append(uInput.upper())
     def gradeExam(self):
+        self.gradesQs = []
         __corrA = []
         __corrA.append(self.q1.answer)
         __corrA.append(self.q2.answer)
@@ -76,21 +77,24 @@ class exam(question):
         for i in range(len(self.userAns)):
             if self.userAns[i] == __corrA[i]:
                 self.counter+=1
+                self.gradesQs.append(1)
             else:
+                self.gradesQs.append(0)
                 continue
     def setExamScore(self):
         self.grade = self.counter
     def returnExamScore(self):
         return self.grade
-
 class student(exam):
-    def __init__(self,id="xxxx",currentScore=0,highestScore=0):
+    def __init__(self,name="",id="xxxx",currentScore=0,highestScore=0):
         self.id = id
         self.currentScore = currentScore
         self.highestScore = highestScore
+        self.name = name
     def setID(self, id):
         self.id = id
-    
+    def returnName(self):
+        return self.name
     def takeQuiz(self):
         e = exam()
         self.makeExam()
@@ -104,13 +108,337 @@ class student(exam):
         return str(self.currentScore) + "/5\n"
     def returnHighest(self):
         return str(self.highestScore) + "/5\n"
+    def returnIndGrades(self):
+        return self.gradesQs
+def main():
+    while True:
+        inp = input("A:student\nB:exit")
+        if inp.upper() == "B":
+            break
+        elif inp.upper() == "A":
+            user = input("Please enter your name ")
+            id = input("Please enter your 4 digit ID to take the exam: ")
+            s = student(user,id)
+            s.takeQuiz()
+            print(s.returnCurrent())
+            print(s.returnHighest())
+            print(s.returnIndGrades())
+            
+            
+        else:
+            print("Enter a valid input!")
+            
+    
+    
+main()
 
-s = student(1234)
-s.takeQuiz()
-print(s.returnCurrent())
-print(s.returnHighest())
-# print(e.q1+e.q2)
-# e.takeExam()
-# e.gradeExam()
-# e.setExamScore()
-# print(e.returnExamScore())
+'''
+A:student
+B:exitA
+Please enter your name alice
+Please enter your 4 digit ID to take the exam: 1234
+In computer jargon, RAM refers to
+(A) Read Only Menu
+(B) Random Access Memory
+(C) Random Accent Memory
+(D) Read Access Memory b
+What do we call a network whose elements may be separated by some distance? It usually involves two or more small networks and dedicated high-speed telephone lines.
+(A) URL (Universal Resource Locator)
+(B) LAN (Local Area Network)
+(C) WAN (Wide Area Network)
+(D) World Wide Web b
+'DB' computer abbreviation usually means?
+(A) Database
+(B) Double Byte
+(C) Data Block
+(D) Driver Boot a
+Which of the following is not a computer language?
+(A) Windows 98
+(B) PASCAL
+(C) FORTRAN
+(D) C++ a
+'DB' computer abbreviation usually means?
+(A) Database
+(B) Double Byte
+(C) Data Block
+(D) Driver Boot a
+current: 4/5
+
+highest: 4/5
+
+[1, 0, 1, 1, 1]
+
+Would you like to take it again
+A: yes
+B: no
+B
+
+test2: 
+A:student
+B:exitA
+Please enter your name mark
+Please enter your 4 digit ID to take the exam: 1234
+In computer jargon, RAM refers to
+(A) Read Only Menu
+(B) Random Access Memory
+(C) Random Accent Memory
+(D) Read Access Memory b
+What do we call a network whose elements may be separated by some distance? It usually involves two or more small networks and dedicated high-speed telephone lines.
+(A) URL (Universal Resource Locator)
+(B) LAN (Local Area Network)
+(C) WAN (Wide Area Network)
+(D) World Wide Web b
+'DB' computer abbreviation usually means?
+(A) Database
+(B) Double Byte
+(C) Data Block
+(D) Driver Boot a
+Which of the following is not a computer language?
+(A) Windows 98
+(B) PASCAL
+(C) FORTRAN
+(D) C++ a
+'DB' computer abbreviation usually means?
+(A) Database
+(B) Double Byte
+(C) Data Block
+(D) Driver Boot a
+current: 4/5
+
+highest: 4/5
+
+[1, 0, 1, 1, 1]
+
+Would you like to take it again
+A: yes
+B: no
+A
+
+In computer jargon, RAM refers to
+(A) Read Only Menu
+(B) Random Access Memory
+(C) Random Accent Memory
+(D) Read Access Memory b
+What do we call a network whose elements may be separated by some distance? It usually involves two or more small networks and dedicated high-speed telephone lines.
+(A) URL (Universal Resource Locator)
+(B) LAN (Local Area Network)
+(C) WAN (Wide Area Network)
+(D) World Wide Web b
+'DB' computer abbreviation usually means?
+(A) Database
+(B) Double Byte
+(C) Data Block
+(D) Driver Boot a
+Which of the following is not a computer language?
+(A) Windows 98
+(B) PASCAL
+(C) FORTRAN
+(D) C++ a
+'DB' computer abbreviation usually means?
+(A) Database
+(B) Double Byte
+(C) Data Block
+(D) Driver Boot a
+current: 4/5
+
+highest: 4/5
+
+[1, 0, 1, 1, 1]
+
+Would you like to take it again
+A: yes
+B: no
+A
+
+In computer jargon, RAM refers to
+(A) Read Only Menu
+(B) Random Access Memory
+(C) Random Accent Memory
+(D) Read Access Memory b
+What do we call a network whose elements may be separated by some distance? It usually involves two or more small networks and dedicated high-speed telephone lines.
+(A) URL (Universal Resource Locator)
+(B) LAN (Local Area Network)
+(C) WAN (Wide Area Network)
+(D) World Wide Web b
+'DB' computer abbreviation usually means?
+(A) Database
+(B) Double Byte
+(C) Data Block
+(D) Driver Boot a
+Which of the following is not a computer language?
+(A) Windows 98
+(B) PASCAL
+(C) FORTRAN
+(D) C++ a
+'DB' computer abbreviation usually means?
+(A) Database
+(B) Double Byte
+(C) Data Block
+(D) Driver Boot a
+current: 4/5
+
+highest: 4/5
+
+[1, 0, 1, 1, 1]
+
+Would you like to take it again
+A: yes
+B: no
+B
+
+test3:
+A:student
+B:exita
+Please enter your name bob
+Please enter your 4 digit ID to take the exam: 1234
+Who developed Yahoo?
+(A) Dennis Ritchie & Ken Thompson
+(B) David Filo & Jerry Yang
+(C) Vint Cerf & Robert Kahn
+(D) Steve Case & Jeff Bezos b
+In computer jargon, RAM refers to
+(A) Read Only Menu
+(B) Random Access Memory
+(C) Random Accent Memory
+(D) Read Access Memory c
+ '.INI' extension refers usually to what kind of file?
+(A) Image file
+(B) System file
+(C) Hypertext related file
+(D) Image Color Matching Profile file d
+ '.INI' extension refers usually to what kind of file?
+(A) Image file
+(B) System file
+(C) Hypertext related file
+(D) Image Color Matching Profile file a
+The Central Processing Unit is an embedded chip that acts as the 'brains' of a computer. What Intel chip was used in the Altair (the first real personal computer)?
+(A) 6502
+(B) 8080
+(C) 6400
+(D) 8286 a
+1/5
+
+1/5
+
+[1, 0, 0, 0, 0]
+
+Would you like to take it again
+A: yes
+B: no
+A
+Who developed Yahoo?
+(A) Dennis Ritchie & Ken Thompson
+(B) David Filo & Jerry Yang
+(C) Vint Cerf & Robert Kahn
+(D) Steve Case & Jeff Bezos b
+In computer jargon, RAM refers to
+(A) Read Only Menu
+(B) Random Access Memory
+(C) Random Accent Memory
+(D) Read Access Memory c
+ '.INI' extension refers usually to what kind of file?
+(A) Image file
+(B) System file
+(C) Hypertext related file
+(D) Image Color Matching Profile file d
+ '.INI' extension refers usually to what kind of file?
+(A) Image file
+(B) System file
+(C) Hypertext related file
+(D) Image Color Matching Profile file a
+The Central Processing Unit is an embedded chip that acts as the 'brains' of a computer. What Intel chip was used in the Altair (the first real personal computer)?
+(A) 6502
+(B) 8080
+(C) 6400
+(D) 8286 a
+1/5
+
+1/5
+
+[1, 0, 0, 0, 0]
+
+Would you like to take it again
+A: yes
+B: no
+B
+test4
+A:student
+B:exita
+Please enter your name web
+Please enter your 4 digit ID to take the exam: 1234
+What do we call a network whose elements may be separated by some distance? It usually involves two or more small networks and dedicated high-speed telephone lines.
+(A) URL (Universal Resource Locator)
+(B) LAN (Local Area Network)
+(C) WAN (Wide Area Network)
+(D) World Wide Web a
+'DB' computer abbreviation usually means?
+(A) Database
+(B) Double Byte
+(C) Data Block
+(D) Driver Boot a
+Who co-created the UNIX operating system in 1969 with Dennis Ritchie?
+(A) Bjarne Stroustrup
+(B) Steve Wozniak
+(C) Ken Thompson
+(D) Niklaus Wirth a
+What is FMD?
+(A) Fast-Ethernet Measuring Device
+(B) Flashing Media Diode
+(C) Fluorescent Multi-Layer Disc
+(D) Flash Media Driver a
+'.BAT' extension refers usually to what kind of file?
+(A) Compressed Archive file
+(B) System file
+(C) Audio file
+(D) Backup file a
+1/5
+
+1/5
+
+[0, 1, 0, 0, 0]
+
+would you like to take it again
+A: yes
+B: no
+B
+
+test5
+A:student
+B:exita
+Please enter your name ned
+Please enter your 4 digit ID to take the exam: 1234
+'DB' computer abbreviation usually means?
+(A) Database
+(B) Double Byte
+(C) Data Block
+(D) Driver Boot a
+What is the term to ask the computer to put information in order numerically or alphabetically?
+(A) Crop
+(B) Report
+(C) Record
+(D) Sort b
+ 'OS' computer abbreviation usually means?
+(A) Order of Significance
+(B) Open Software
+(C) Operating System
+(D) Optical Sensor c
+What is the term to ask the computer to put information in order numerically or alphabetically?
+(A) Crop
+(B) Report
+(C) Record
+(D) Sort d
+What do we call a network whose elements may be separated by some distance? It usually involves two or more small networks and dedicated high-speed telephone lines.
+(A) URL (Universal Resource Locator)
+(B) LAN (Local Area Network)
+(C) WAN (Wide Area Network)
+(D) World Wide Web b
+3/5
+
+3/5
+
+[1, 0, 1, 1, 0]
+would you like to take it again
+A:yes
+B:no
+B
+'''
